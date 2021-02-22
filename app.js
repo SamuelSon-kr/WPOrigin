319beb5264a00cbf1080737985eda3df811bf3a9
SON SUNGWOO <rg3120v@gmail.com>
2021-02-22 14:17:22 +0900

    origin

---
+++ app.js
@@ -0,0 +1,8 @@
+const toggleBtn = document.querySelector('.naver_toggleBtn');
+const menu = document.querySelector('.navbar_menu');
+// const sns = documnet.querySelector('.navbar_sns');
+
+// toggleBtn.addEventListener('cleck', () =>{
+//   menu.classlist.toggle('active');
+//   sns.classlist.toggle('active');
+// });
